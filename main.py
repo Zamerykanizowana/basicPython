27aeda30a62f89208ccdfeaa9e4e4a105f5596f5
Aleksanda Wojewoda <a.wojewoda18@gmail.com>
2022-03-01 17:42:22 +0100

    Add test no. 1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def srednia(tablica_ocen, kolo_naukowe):
     suma += ocena
   srednia = suma / len(tablica_ocen)
   if kolo_naukowe == "True":
+      srednia = srednia
     # TODO dodaj do sredniej 0,5 osceny jesli jest dzialalnosc w kole
   return srednia
 
@@ -18,3 +19,15 @@ def isNaN(cos):
     return False
   else:
     return True
+
+
+# Test no 1
+student = ["Monika", "Testowa", "111222", 2,2,3,4,5,2,5,5, "True", "False", 2]
+student2 = ["Aleksandra", "Testowa", "111333", 2,2,3,4,5,2,5,5, "False", "False", 2]
+studenci = [student, student2]
+if srednia(student[3:11], student[11]) == 3.5 and srednia(student2[3:11], student2[11]) == 4.0:
+    print("test pozytywny")
+else:
+    print("test negatywny")
+    print(str_srednia_studenta(student[0], student[1], srednia(student[3:11],student[11])))
+    print(str_srednia_studenta(student2[0], student2[1], srednia(student2[3:11],student2[11])))
